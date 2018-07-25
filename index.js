03c3f2e3e2521ad305026a3ad3ec5191db2fbe9a
Your Name <you@example.com>
2018-07-25 19:50:49 +0530

    my first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+$(document).ready(() =>{
+        $('#show').click(()=>{
+        
+        	$("#data1").show();
+        	$("#data2").show();
+        });
+         $('#hide').click(()=>{
+        
+        	$("#data1").hide();
+        	$("#data2").hide();       
+        });
+       }) ;
